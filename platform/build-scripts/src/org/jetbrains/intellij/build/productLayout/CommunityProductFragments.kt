46d4ae465dd5c3e5b5c55c1ba221807660ac6c6c
Nikolai Rykunov <rykunov@pm.me>
2026-03-14 00:39:50 +0100

    [initial-clean] Get rid of the java plugin (where it is not needed for build scripts)

--- platform/build-scripts/src/org/jetbrains/intellij/build/productLayout/CommunityProductFragments.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/productLayout/CommunityProductFragments.kt
@@ -27,9 +27,6 @@ object CommunityProductFragments {
 
     // Optional plugin support (with fallback)
     deprecatedInclude("intellij.platform.remoteServers.impl", "intellij.platform.remoteServers.impl.xml", optional = true)
-
-    // Extensions block (UTM tracking, new UI onboarding)
-    deprecatedInclude("intellij.java.ide.resources", "META-INF/JavaIdePlugin.xml")
   }
 
   /**
