007ec1e2720b9303de54b837dd5e2fbefea5917c
Nikolai Rykunov <rykunov@pm.me>
2026-03-14 01:07:26 +0100

    [initial-cleanup] Cleanup plugins' sources

--- plugins/ui-designer/testData/sourceCodeGenerator/buttonGroupNoMembers/BindingTest.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class BindingTest {
-  private JPanel myPanel;
-  private ButtonGroup myButtonGroup;
-
-  public BindingTest() {
-  }
-}
--- plugins/ui-designer/testData/sourceCodeGenerator/noI18nTextProperty/BindingTest.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class BindingTest {
-  public JComponent myRootComponent;
-  private JLabel myLabel;
-
-  public BindingTest() {
-  }
-}
